804d4e6e940bfcf171a7a7f4422faae1ff0dad65
Alexandr Rubis <stem.201496@gmail.com>
2017-07-07 14:31:49 +0300

    add products

--- MEGA/Controllers/HomeController.cs
+++ MEGA/Controllers/HomeController.cs
@@ -16,32 +16,36 @@ namespace MEGA.Controllers
             {
                 ViewBag.Advertisings = context.Advertisings.ToList();
                 List<Product> pr = new List<Product>();
-                
-                ViewBag.one = context.Products.Where(x => x.GoodsTypeId == 1).ToList();
-                ViewBag.one1 = context.Products.Where(x => x.GoodsTypeId == 2).ToList();
-                ViewBag.one2 = context.Products.Where(x => x.GoodsTypeId == 3).ToList();
+                var rand = new Random();
+                rand.Next();
+                ViewBag.pechat1 = context.GoodsTypes.Where(x => x.Id == 1).ToList();
+                ViewBag.pechat2 = context.GoodsTypes.Where(x => x.Id == 2).ToList();
+                ViewBag.pechat3 = context.GoodsTypes.Where(x=>x.Id == 3).ToList();
                 ViewBag.one3 = context.Products.Where(x => x.GoodsTypeId == 4).ToList();
                 ViewBag.one4 = context.Products.Where(x => x.GoodsTypeId == 5).ToList();
-                ViewBag.one5 = context.Products.Where(x => x.GoodsTypeId == 6).ToList();
-                ViewBag.one6 = context.Products.Where(x => x.GoodsTypeId == 7).ToList();
-                ViewBag.one7 = context.Products.Where(x => x.GoodsTypeId == 8).ToList();
-                ViewBag.one8 = context.Products.Where(x => x.GoodsTypeId == 9).ToList();
-                ViewBag.one9 = context.Products.Where(x => x.GoodsTypeId == 10).ToList();
-                ViewBag.one10 = context.Products.Where(x => x.GoodsTypeId == 11).ToList();
-                ViewBag.one11 = context.Products.Where(x => x.GoodsTypeId == 12).ToList();
-                ViewBag.one12 = context.Products.Where(x => x.GoodsTypeId == 13).ToList();
-                ViewBag.one13 = context.Products.Where(x => x.GoodsTypeId == 14).ToList();
-                ViewBag.one14 = context.Products.Where(x => x.GoodsTypeId == 15).ToList();
-                ViewBag.one15 = context.Products.Where(x => x.GoodsTypeId == 16).ToList();
-                ViewBag.one16 = context.Products.Where(x => x.GoodsTypeId == 17).ToList();
-                ViewBag.one17 = context.Products.Where(x => x.GoodsTypeId == 18).ToList();
-                ViewBag.one18 = context.Products.Where(x => x.GoodsTypeId == 19).ToList();
-                ViewBag.one19 = context.Products.Where(x => x.GoodsTypeId == 20).ToList();
-                ViewBag.one20 = context.Products.Where(x => x.GoodsTypeId == 21).ToList();
-                ViewBag.one21 = context.Products.Where(x => x.GoodsTypeId == 22).ToList();
-                ViewBag.one22 = context.Products.Where(x => x.GoodsTypeId == 23).ToList();
+                ViewBag.suvenir = context.GoodsTypes.ToList();
 
                 return View();
+            }
+            
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult AllDeteils(int id)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return View(context.Products.Where(x=>x.GoodsTypeId == id).ToList());
+            }
+            
+        }
+        public ActionResult Ditails(int id)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return View(context.Products.Where(x=>x.Id == id).ToList());
             }
             
         }
